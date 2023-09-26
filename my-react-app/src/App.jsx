19ce4cd41bcf9a690d333924790e5de39be4e4cf
shameemfrhn <shameemfarhan.sfk@gmail.com>
2023-09-26 23:38:22 +0530

    1st

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Counter from './counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App with Hooks</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  // Define a state variable 'count' and a function 'setCount' to update it.
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
